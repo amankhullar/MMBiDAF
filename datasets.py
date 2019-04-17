a083df9049ae610df4bd6936f7b42fcbd21d4937
Udit Arora <uditara09@gmail.com>
2019-04-17 19:57:47 +0530

    Updated ImageDataset to load images for all videos of a course

--- datasets.py
+++ datasets.py
@@ -9,15 +9,22 @@ class ImageDataset(Dataset):
     """
     A PyTorch dataset class to be used in the PyTorch DataLoader to create batches.
     """
-    def __init__(self, image_dir, transform = None):
+    def __init__(self, images_dir, transform = None):
         """
         Args:
-            image_dir (string) : Directory with all the train images
-            TODO : include all the training data at one place.
+            images_dir (string) : Directory with all the train images
+                                  for all the vidoes
+            TODO : include all the training data (all modalities) at one place
         """
-        self.image_dir = image_dir
+        self.images_dir = images_dir
         self.transform = transform
-        self.images = [img for img in os.listdir(self.image_dir) if os.path.isfile(os.path.join(self.image_dir, img))]
+        self.load_images()
+    
+    def load_images(self):
+        for video_path in os.listdir(self.images_dir):
+            keyframes = [img for img in os.listdir(os.path.join(self.image_dir, video_path) \
+                         if os.path.isfile(os.path.join(self.image_dir, video_path, img))]
+            self.images.extend(keyframes)
 
     def __len__(self):
         return len(os.listdir(self.image_dir))
