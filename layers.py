032b7f870fa58e3553acbb84602a76a99530c148
Aman Khullar <amankhullar97@gmail.com>
2019-04-24 16:53:53 +0530

    masked_softmax method added only for debuggin purpose

--- layers.py
+++ layers.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from util import masked_softmax
+# from util import masked_softmax
 
 class TextEmbedding(nn.Module):
     """
@@ -357,3 +357,24 @@ class TextAudioBiDAFAttention(nn.Module):
 
         return s
 
+def masked_softmax(logits, mask, dim=-1, log_softmax=False):
+    """Take the softmax of `logits` over given dimension, and set
+    entries to 0 wherever `mask` is 0.
+
+    Args:
+        logits (torch.Tensor): Inputs to the softmax function.
+        mask (torch.Tensor): Same shape as `logits`, with 0 indicating
+            positions that should be assigned 0 probability in the output.
+        dim (int): Dimension over which to take softmax.
+        log_softmax (bool): Take log-softmax rather than regular softmax.
+            E.g., some PyTorch functions such as `F.nll_loss` expect log-softmax.
+
+    Returns:
+        probs (torch.Tensor): Result of taking masked softmax over the logits.
+    """
+    mask = mask.type(torch.float32)
+    masked_logits = mask * logits + (1 - mask) * -1e30
+    softmax_fn = F.log_softmax if log_softmax else F.softmax
+    probs = softmax_fn(masked_logits, dim)
+
+    return probs
