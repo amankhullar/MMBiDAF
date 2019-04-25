16e0854d9b702d6722efe357e6f01c60c3e5da9b
Udit Arora <uditara09@gmail.com>
2019-04-25 13:01:42 +0530

    Updated ImageDataset to load keyframe images across all videos of all courses

--- datasets.py
+++ datasets.py
@@ -39,38 +39,55 @@ class TextDataset(Dataset):
 class ImageDataset(Dataset):
     """
     A PyTorch dataset class to be used in the PyTorch DataLoader to create batches.
+
+    Member variables:
+    self.image_paths (2D list) : A 2D list containing image paths of all the videos.
+                                 The first index represents the video, and the
+                                 second index represents the keyframe.
+    self.num_videos (int) : The total number of videos across courses in the dataset.
+
     """
-    def __init__(self, images_dir, transform = None):
+    def __init__(self, courses_dir, transform = None):
         """
         Args:
-            images_dir (string) : Directory with all the train images
-                                  for all the videos
+            courses_dir (string) : Directory with all the courses
             transform (torchvision.transforms.transforms.Compose) : The required transformation required to normalize all images
         """
-        self.images_dir = images_dir
+        self.courses_dir = courses_dir
         self.transform = transform
-        self.sorted_image_dir = sorted(os.listdir(self.images_dir), key = int)
-        self.images = self.load_images()
+        self.num_videos = 0
+        self.image_paths = self.load_image_paths()
 
     def get_num(self, str):
         return int(re.search(r'\d+', re.search(r'_\d+', str).group()).group())
     
-    def load_images(self):
+    def load_image_paths(self):
         images = []
-        for video_path in self.sorted_image_dir:
-            keyframes = [os.path.join(video_path, img) for img in os.listdir(os.path.join(self.images_dir, video_path)) \
-                         if os.path.isfile(os.path.join(self.images_dir, video_path, img))]
-            keyframes.sort(key = self.get_num)
-            images.extend([keyframes])
+
+        # Get sorted list of all courses (excluding any files)
+        dirlist = []
+        for fname in os.listdir(self.courses_dir):
+            if os.path.isdir(os.path.join(self.courses_dir, fname)):
+                dirlist.append(fname)
+
+        for course_dir in sorted(dirlist, key=int):
+            keyframes_dir_path = os.path.join(self.courses_dir, course_dir, 'video_key_frames/')
+            for video_dir in sorted(os.listdir(keyframes_dir_path), key=int):
+                self.num_videos += 1
+                video_dir_path = os.path.join(keyframes_dir_path, video_dir)
+                keyframes = [os.path.join(video_dir_path, img) for img in os.listdir(video_dir_path) \
+                            if os.path.isfile(os.path.join(video_dir_path, img))]
+                keyframes.sort(key = self.get_num)
+                images.extend([keyframes])
+
         return images
 
     def __len__(self):
-        return len(os.listdir(self.images_dir))
+        return self.num_videos
 
     def __getitem__(self, idx):
         transformed_images = []
-        for image_name in self.images[idx]:
-            image_path = os.path.join(self.images_dir, image_name)
+        for image_path in self.image_paths[idx]:
             image = Image.open(image_path)
             if self.transform is not None:
                 image = self.transform(image)
