dd509ba187d78704a6d4099383c7e7e7bd15f384
Aman Khullar <amankhullar97@gmail.com>
2019-08-22 20:39:31 +0530

    minor bugs resolved

--- train.py
+++ train.py
@@ -130,7 +130,7 @@ def main(course_dir, text_embedding_size, audio_embedding_size, image_embedding_
         with torch.enable_grad(), tqdm(total=len(train_text_loader.dataset)) as progress_bar:
             for (batch_text, original_text_lengths), (batch_audio, original_audio_lengths), (batch_images, original_img_lengths), (batch_target_indices, batch_source_paths, batch_target_paths, original_target_len) in zip(train_text_loader, train_audio_loader, train_image_loader, train_target_loader):
                 loss = 0
-                max_dec_len = torch.max(original_target_len)             # TODO check error : max decoder timesteps for each batch 
+                max_dec_len = max(original_target_len)             # TODO check error : max decoder timesteps for each batch 
 
                 if not USE_CPU:
                     # Transfer tensors to GPU
@@ -192,9 +192,9 @@ def main(course_dir, text_embedding_size, audio_embedding_size, image_embedding_
                     ema.resume(model)
 
                 # Generate summary
-                print('Generated summary for iteration {}: '.format(epoch))
-                summaries = evaluate.get_generated_summaries(batch_out_distributions, original_text_lengths, batch_source_paths)
-                print(summaries)
+                # print('Generated summary for iteration {}: '.format(epoch))
+                # summaries = evaluate.get_generated_summaries(batch_out_distributions, original_text_lengths, batch_source_paths)
+                # print(summaries)
 
                 if SINGLE:
                     break
